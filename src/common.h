253359bf3c7559a736916dbc8027d9746cc6dcfa
Jens Kallup <kallup-dev@web.de>
2023-07-07 17:13:03 +0200

    update

--- src/common.h
+++ src/common.h
@@ -3,12 +3,35 @@
 // Author : Jens Kallup (c) 2023-07.01
 // License: all rights reserved.
 // ----------------------------------------------------------------------------
-#ifndef COMMON_H
-#define COMMON_H
+#ifndef COMMON_H     // old fashion
+# define COMMON_H 1  // safety guardian
+# pragma once        // only one include
+
+// ----------------------------------------------------------------------------
+// import/export stuff ...
+// ----------------------------------------------------------------------------
+#if defined(_MSC_VER)
+    //  Microsoft 
+    # define EXPORT __declspec(dllexport)
+    # define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    # define EXPORT __attribute__((visibility("default")))
+    # define IMPORT
+#else
+    //  do nothing and hope for the best?
+    # define EXPORT
+    # define IMPORT
+    # pragma warning Unknown dynamic link import/export semantics.
+#endif
 
 // ----------------------------------------------------------------------------
 // standard i/o header proto type stuff:
 // ----------------------------------------------------------------------------
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 # include <stdio.h>
 # include <string.h>
 # include <math.h>
@@ -20,44 +43,54 @@
 // ----------------------------------------------------------------------------
 # include <windows.h>
 
+#ifdef __cplusplus
+};
+#endif
+
 # define YY_NO_UNISTD_H
 
-# define false 0
-# define true  1
+int inline false() { return 0; }
+int inline true () { return 1; }
 
 extern char* dBaseDLLdirectory;
 
 // ----------------------------------------------------------------------------
 // parser stuff ...
 // ----------------------------------------------------------------------------
-extern char* yy_number;
-extern char* yy_text;
+extern char* EXPORT yy_number;
+extern char* EXPORT yy_text;
 
-extern int   yy_col;
-extern int   yy_row;
+extern int   EXPORT yy_col;
+extern int   EXPORT yy_row;
 
-extern void pascalerror(char* m);
-extern int  pascalwrap(void);
-extern int  count(void);
+// ----------------------------------------------------------------------------
+// pascal stuff:
+// ----------------------------------------------------------------------------
+extern void  EXPORT pascalerror(char* m);
+extern int   EXPORT pascalwrap(void);
+extern int   EXPORT count(void);
 
-extern void yy_pascal_lex_parser_error(void (*func)(char*));
-extern void yy_pascal_lex_close(void);
-extern int  yy_pascal_lex_get_line(void);
-extern int  yy_pascal_lex_getlines(void);
-extern BOOL yy_pascal_lex_main(char*,char*);
+extern void  EXPORT yy_pascal_lex_parser_error(void (*func)(char*));
+extern void  EXPORT yy_pascal_lex_close(void);
+extern int   EXPORT yy_pascal_lex_get_line(void);
+extern int   EXPORT yy_pascal_lex_getlines(void);
+extern BOOL  EXPORT yy_pascal_lex_main(char*,char*);
 
-extern void yy_pascal_fatal_error(char* message);
+extern void  EXPORT yy_pascal_fatal_error(char* message);
 
-//
-extern void dbaseerror(char* m);
-extern int  dbasewrap(void);
+// ----------------------------------------------------------------------------
+// dbase stuff:
+// ----------------------------------------------------------------------------
+extern void  EXPORT dbaseerror(char* m);
+extern int   EXPORT dbasewrap(void);
 
-extern void yy_dbase_lex_parser_error(void (*func)(char*));
-extern void yy_dbase_lex_close(void);
-extern int  yy_dbase_lex_get_line(void);
-extern int  yy_dbase_lex_getlines(void);
-extern BOOL yy_dbase_lex_main(char*,char*);
+extern void  EXPORT yy_dbase_lex_parser_error(void (*func)(char*));
+extern void  EXPORT yy_dbase_lex_close(void);
+extern int   EXPORT yy_dbase_lex_get_line(void);
+extern int   EXPORT yy_dbase_lex_getlines(void);
+extern BOOL  EXPORT yy_dbase_lex_main(char*,char*);
 
-extern void yy_dbase_fatal_error(char* message);
-#endif
+extern void  EXPORT yy_dbase_fatal_error(char* message);
+
+#endif  // COMMON_H
 
