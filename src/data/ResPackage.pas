150cc2878f038411db2a93d7c7302d27d2a09fea
Jens Kallup <kallup-dev@web.de>
2023-08-22 12:05:31 +0200

    update

---
+++ src/data/ResPackage.pas
@@ -0,0 +1,30 @@
+// ----------------------------------------------
+// THIS FILE IS CREATED AUTOMATICALLY
+// (c) 2023 ResBuilder v1.0
+// by paule32 - Jens Kallup - non-profit Software
+// ----------------------------------------------
+unit ResourceFile;
+interface
+uses
+  RF_1, RF_2, RF_3, RF_4, RF_5, RF_6, RF_7, 
+  RF_8, RF_9, RF_10, RF_11, RF_12, RF_13, RF_14, 
+  RF_15, RF_16, RF_17, RF_18, RF_19, RF_20, 
+  RF_21, RF_22, RF_23, RF_24, RF_25, RF_26, 
+  RF_27, RF_28, RF_29, RF_30, RF_31, RF_32, 
+  RF_33, RF_34, RF_35, RF_36, RF_37, RF_38, 
+  RF_39, RF_40, RF_41, RF_42, RF_43, RF_44, 
+  RF_45, RF_46, RF_47, RF_48, RF_49, RF_50, 
+  RF_51, RF_52, RF_53, RF_54, RF_55, RF_56, 
+  RF_57, RF_58, RF_59, RF_60, RF_61, RF_62, 
+  RF_63, RF_64, RF_65, RF_66, RF_67, RF_68, 
+  RF_69, RF_70, RF_71, RF_72, RF_73, RF_74, 
+  RF_75, RF_76, RF_77, RF_78, RF_79, RF_80, 
+  RF_81, RF_82, RF_83, RF_84, RF_85, RF_86, 
+  RF_87, RF_88, RF_89, RF_90, RF_91, RF_92, 
+  RF_93, RF_94, RF_95, RF_96, RF_97, RF_98, 
+  RF_99, RF_100, RF_101, RF_102, RF_103, RF_104, 
+  RF_105, ;
+  // you have to remove the last comma !
+
+implementation
+end.
